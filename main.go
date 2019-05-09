348611d63acbaacb61197a9f54501d192dd9cb1c
Andy Swanson <11773096+granite-cloud@users.noreply.github.com>
2019-05-09 14:35:55 -0400

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World Test 3"}
+		res := &response{Message: "Hello World Test 4"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
